ea441c3124114cbe983d514cecd98377d219ad43
PA-GAGNAIRE <43635233+PA-GAGNAIRE@users.noreply.github.com>
2021-08-17 15:07:10 +0200

    Update genome_data.py

--- stdpopsim/catalog/GenSpe/genome_data.py
+++ stdpopsim/catalog/GenSpe/genome_data.py
@@ -1,1 +1,32 @@
-
+# File manually generated.
+#
+data = {
+    "assembly_accession": NA,
+    "assembly_name": "GCA_000689215.1",
+    "chromosomes": {
+        "LG1A": {"length": 29036788, "synonyms": []},
+        "LG1B": {"length": 17995764, "synonyms": []},
+        "LG2": {"length": 26218737, "synonyms": []},
+        "LG3": {"length": 13451259, "synonyms": []},
+        "LG4": {"length": 27569954, "synonyms": []},
+        "LG5": {"length": 32612477, "synonyms": []},
+        "LG6": {"length": 28185029, "synonyms": []},
+        "LG7": {"length": 28480941, "synonyms": []},
+        "LG8": {"length": 23067096, "synonyms": []},
+        "LG9": {"length": 22362490, "synonyms": []},
+        "LG10": {"length": 24053896, "synonyms": []},
+        "LG11": {"length": 26215200, "synonyms": []},
+        "LG12": {"length": 23234908, "synonyms": []},
+        "LG13": {"length": 27622843, "synonyms": []},
+        "LG14": {"length": 28395245, "synonyms": []},
+        "LG15": {"length": 25566315, "synonyms": []},
+        "LG16": {"length": 25821574, "synonyms": []},
+        "LG17": {"length": 22893611, "synonyms": []},
+        "LG18-21": {"length": 16453912, "synonyms": []},
+        "LG19": {"length": 23202889, "synonyms": []},
+        "LG20": {"length": 28398722, "synonyms": []},
+        "LG22-25": {"length": 26439989, "synonyms": []},
+        "LG24": {"length": 13918872, "synonyms": []},
+        "LGx": {"length": 17779090, "synonyms": []},
+    },
+}
